0a32c9ce40293ecde326a2ec959dc5ee5797ff4f
Alex <alexpetit12@hotmail.com>
2015-08-01 22:40:36 -0400

    Added Lab 03 solution

---
+++ Lab03/Lab03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // convert fahrenheit to celsius
+            float originalFahrenheit;
+
+            // get fahrenheit temperature from user
+            Console.WriteLine("Enter temperature in Fahrenheit: ");
+            originalFahrenheit = float.Parse(Console.ReadLine());
+
+            // convert fahrenheit to celsius
+            float celsius = (originalFahrenheit - 32) * 5 / 9;
+
+            // print result
+            Console.WriteLine(originalFahrenheit + " degrees Fahrenheit is equivalent to " + celsius + " degrees Celsius.");
+            Console.WriteLine();
+        }
+    }
+}
