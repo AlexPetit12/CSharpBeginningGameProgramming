3a300c1f936873bb76a987cafaede542fdee0001
Alex <alexpetit12@hotmail.com>
2015-08-01 22:40:23 -0400

    Added Lab 01 solution

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // output your name
+            Console.WriteLine("Alexandre");
+            Console.WriteLine();
+
+            // output your nemesis
+            Console.WriteLine("None!");
+            Console.WriteLine();
+
+            // output your 3 favorite games
+            Console.WriteLine("I haven't played video games in a while!");
+            Console.WriteLine();
+        }
+    }
+}
