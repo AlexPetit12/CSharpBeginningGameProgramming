0e6bc8099eedf5bf189625a374619a7b4b5a4ab6
Alex <alexpetit12@hotmail.com>
2015-08-01 22:40:43 -0400

    Added Lab 04 solution

---
+++ Lab04/Lab04/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab4
+{
+	/// <summary>
+	/// Demonstrates classes and objects
+	/// </summary>
+    class Program
+    {
+		/// <summary>
+		/// Demonstrates use of Deck and Card objects
+		/// </summary>
+		/// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // create a new deck and print the contents of the deck
+            Deck deck = new Deck();
+
+            // shuffle the deck and print the contents of the deck
+            deck.Shuffle();
+            deck.Print();
+
+            Console.WriteLine();
+
+            // take the top card from the deck and print the card rank and suit
+            Card topCard = deck.TakeTopCard();
+
+            Console.WriteLine("Top card: " + topCard.Rank + " of " + topCard.Suit + ".");
+            Console.WriteLine();
+
+            // take the top card from the deck and print the card rank and suit
+            topCard = deck.TakeTopCard();
+
+            Console.WriteLine("Top card: " + topCard.Rank + " of " + topCard.Suit + ".");
+            Console.WriteLine();
+        }
+    }
+}
