4c41a4908221b35ce26a8277d02838cf5c805bb6
Alex <alexpetit12@hotmail.com>
2015-08-01 22:40:29 -0400

    Added Lab 02 solution

---
+++ Lab02/Lab02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print my age
+            int age = 22;
+
+            Console.WriteLine("I am " + age + " years old.");
+            Console.WriteLine();
+
+            // output percent score
+            const int MAX_SCORE = 100;
+            int score = 95;
+
+            float percent = (float)score / MAX_SCORE;
+
+            Console.WriteLine("Percent score is " + percent + ".");
+            Console.WriteLine();
+        }
+    }
+}
